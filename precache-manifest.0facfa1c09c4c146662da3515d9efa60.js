bf79fc7901629e91b1bc46856a3a02f6b4b9da27
Astrid Zapata G <astrid.zapata.780@unisabaneta.edu.co>
2020-08-28 08:27:50 -0500

    Updates

---
+++ precache-manifest.0facfa1c09c4c146662da3515d9efa60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66fac2044d5aa257ad382e1f0f2032ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "350129d4d923a7edbc32",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e6c3aba8de11282b1382",
+    "url": "/static/js/2.09d32d23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09d32d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "350129d4d923a7edbc32",
+    "url": "/static/js/main.1ae106f4.chunk.js"
+  },
+  {
+    "revision": "18706b6d6a42c982038d",
+    "url": "/static/js/runtime-main.608d139f.js"
+  }
+]);
